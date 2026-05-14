2204718e508b6cbb4586bb0e64a70fadac8fad9d
manasse0318 <manasse.tedika.nsumbu@student.ehb.be>
2026-05-14 14:07:11 +0200

    add base css and import from main

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+import "./css/style.css";
 "use strict";
 console.log("AnimeZone is starting...");
+
+fetch("https://api.jikan.moe/v4/top/anime")
+  .then((response)=> response.json())
+  .then((data)=> console.log("Jikan response:", data));
